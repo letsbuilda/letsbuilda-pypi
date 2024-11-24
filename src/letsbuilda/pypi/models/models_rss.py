b6f403cdebf787fbfdebdd45db617cbe5286632c
Bradley Reynolds <bradley.reynolds@darbia.dev>
2024-11-24 03:55:58 +0000

    Raw conversion of dataclasses to models
    Signed-off-by: GitHub <noreply@github.com>

--- src/letsbuilda/pypi/models/models_rss.py
+++ src/letsbuilda/pypi/models/models_rss.py
@@ -1,13 +1,11 @@
 """Models for RSS responses."""
 
-from dataclasses import dataclass
 from datetime import datetime
-from email.utils import parsedate_to_datetime
-from typing import Self
+
+from pydantic import BaseModel
 
 
-@dataclass(frozen=True)
-class RSSPackageMetadata:
+class RSSPackageMetadata(BaseModel):
     """RSS Package metadata."""
 
     title: str
@@ -17,32 +15,3 @@ class RSSPackageMetadata:
     description: str | None
     author: str | None
     publication_date: datetime
-
-    @classmethod
-    def build_from(cls: type[Self], data: dict[str, str]) -> Self:
-        """
-        Build an instance from raw data.
-
-        Parameters
-        ----------
-        data
-            Parsed RSS data from PyPI's RSS API.
-
-        Returns
-        -------
-        RSSPackageMetadata
-            The pacckage metadata from the RSS API.
-        """
-        split_title = data["title"].removesuffix(" added to PyPI").split()
-        title = split_title[0]
-        version = split_title[1] if len(split_title) == 2 else None  # noqa: PLR2004 - is not magic
-
-        return cls(
-            title=title,
-            version=version,
-            package_link=data["link"],
-            guid=data.get("guid"),
-            description=data.get("description"),
-            author=data.get("author"),
-            publication_date=parsedate_to_datetime(data["pubDate"]),
-        )
